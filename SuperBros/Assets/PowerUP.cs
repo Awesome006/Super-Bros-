0871914189428ef4e3e482ee80cee1a7f2e588e3
Awesome006 <48515987+Awesome006@users.noreply.github.com>
2021-08-15 14:41:27 +0530

    Finshed

---
+++ SuperBros/Assets/PowerUP.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUP : MonoBehaviour
+{    
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            GetComponent<CharacterController2D>().Pickup();
+            Destroy(this.gameObject);
+        }
+    }
+   
+}
+
---
+++ SuperBros/Assets/Scripts/CameraShake.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public IEnumerator Shake(float duration, float maginitude)
+    {
+        Vector3 orignalpos = this.transform.localPosition;
+        float elapsed = 0.0f;
+        while (elapsed < duration)
+        {
+            float x = UnityEngine.Random.Range(-1f, 1f) * maginitude;
+            float y = UnityEngine.Random.Range(+1f, -1f) * maginitude;
+            transform.localPosition = new Vector3(x, y, orignalpos.z);
+            elapsed = +Time.deltaTime;
+            yield return null;
+
+        }
+        transform.localPosition = orignalpos;
+
+
+    }
+}
+
+    
